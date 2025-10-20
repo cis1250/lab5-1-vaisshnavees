1080bed84268ba7909728828f41897a311433b5f
vaisshnavee <vsivakol@uoguelph.ca>
2025-10-20 12:54:00 -0400

    Update word_frequency.py

--- word_frequency.py
+++ word_frequency.py
@@ -1,26 +1,72 @@
 #!/usr/bin/env python3
-
 # Word frequency exercise
-# TODO: (Read detailed instructions in the Readme file)
 
 import re
 
-#This is a function that checks if a text qualifies as a sentence. You do not need to modify this!
+# checking if the sentence is good through if 
 def is_sentence(text):
-    # Check if the text is not empty and is a string
+    # no empty spaces
     if not isinstance(text, str) or not text.strip():
         return False
 
-    # Check for starting with a capital letter
+    # starts with capital letters
     if not text[0].isupper():
         return False
 
-    # Check for ending punctuation
+    # punctuation
     if not re.search(r'[.!?]$', text):
         return False
 
-    # Check if it contains at least one word (non-whitespace characters)
+    # has at least one word
     if not re.search(r'\w+', text):
         return False
 
     return True
+
+def get_sentence():#chaning main to get_sequence
+    #user input
+    user_sentence = input("Enter a sentence: ")
+
+    # if not valid
+    while not is_sentence(user_sentence):
+        print("This does not meet the criteria for a sentence.")
+        user_sentence = input("Enter a sentence: ")
+    return user_sentence #adding return 
+
+    def calculate_frequencies(sentence);#adding def
+    
+    cleaned_sentence = user_sentence[:-1].lower()
+    
+    words = cleaned_sentence.split()
+    
+    word_list = []        
+    frequency_list = []   
+    
+    
+    for word in words:
+        # removing puncuation
+        clean_word = word.strip('.,!?;:"')
+        
+        if clean_word in word_list:
+            index = word_list.index(clean_word)
+            frequency_list[index] += 1
+        else:
+            word_list.append(clean_word)
+            frequency_list.append(1)
+    return word_list, frequency_list #adding return
+
+    def print_frequencies(words, frequencies):#adding new def
+    # output
+    print("\nWord frequencies:")
+    for i in range(len(word_list)):
+        print(f"{word_list[i]}: {frequency_list[i]}")
+def main():#creatting new main
+    sentence = get_sentence()
+    #get users sentence
+words,frequencies = calculate_frequencies(sentence)
+#calculate word frequencies
+
+print_frequencies(words, frequencies)#printing results
+# Run the program
+if __name__ == "__main__":
+    main()
